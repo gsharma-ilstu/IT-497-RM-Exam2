4590787f64f3477644e1e5614e97d5343a77852b
gsharma-ilstu <gsharma@ilstu.edu>
2014-10-26 22:37:34 -0500

    Update Exam2Q3P1.R

--- Exam2Q3P1.R
+++ Exam2Q3P1.R
@@ -1,7 +1,6 @@
 library(plyr)
 library(devtools)
 URLaddress3 <- paste0("https://raw.githubusercontent.com/jrwolf/IT497/master/fitstats.csv")
-DataURL3 <- RCurl ::getURL(URLaddress3)
-Ans3 <- read.csv(textConnection(DataURL3),quote = NULL,sep=",")
+DataURL3 <- RCurl::getURL(URLaddress3)
+Ans3 <- read.csv(textConnection(DataURL3),sep=",")
 Ans3 <- Ans3[-541:-542,]
-Ans3
